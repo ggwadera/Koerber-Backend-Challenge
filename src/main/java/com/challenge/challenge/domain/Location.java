1c721131aee6b82e70d29c0ed84411addd62a794
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-07-26 00:56:33 +0100

    Start refactoring to use JPA instead

---
+++ src/main/java/com/challenge/challenge/domain/Location.java
@@ -0,0 +1,59 @@
+package com.challenge.challenge.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "location")
+public class Location {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+
+    @Column(name = "borough")
+    private String borough;
+
+
+    @Column(name = "zone")
+    private String zone;
+
+
+    @Column(name = "service_zone")
+    private String serviceZone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBorough() {
+        return borough;
+    }
+
+    public void setBorough(String borough) {
+        this.borough = borough;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
+    public String getServiceZone() {
+        return serviceZone;
+    }
+
+    public void setServiceZone(String serviceZone) {
+        this.serviceZone = serviceZone;
+    }
+}
---
+++ src/main/java/com/challenge/challenge/domain/Trip.java
@@ -0,0 +1,68 @@
+package com.challenge.challenge.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "trip")
+public class Trip {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "pickup_datetime")
+    private LocalDateTime pickupDateTime;
+
+    @Column(name = "dropoff_datetime")
+    private LocalDateTime dropoffDateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "pickup_location_id")
+    private Location pickupLocation;
+
+    @ManyToOne
+    @JoinColumn(name = "dropoff_location_id")
+    private Location dropoffLocation;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getPickupDateTime() {
+        return pickupDateTime;
+    }
+
+    public void setPickupDateTime(LocalDateTime pickupDateTime) {
+        this.pickupDateTime = pickupDateTime;
+    }
+
+    public LocalDateTime getDropoffDateTime() {
+        return dropoffDateTime;
+    }
+
+    public void setDropoffDateTime(LocalDateTime dropoffDateTime) {
+        this.dropoffDateTime = dropoffDateTime;
+    }
+
+    public Location getPickupLocation() {
+        return pickupLocation;
+    }
+
+    public void setPickupLocation(Location pickupLocation) {
+        this.pickupLocation = pickupLocation;
+    }
+
+    public Location getDropoffLocation() {
+        return dropoffLocation;
+    }
+
+    public void setDropoffLocation(Location dropoffLocation) {
+        this.dropoffLocation = dropoffLocation;
+    }
+
+}

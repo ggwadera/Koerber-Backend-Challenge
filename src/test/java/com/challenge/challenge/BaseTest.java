759cd0093b4a7cb352a58b18b3e8649c14a1f88e
Guilherme Gwadera <guilherme.gwadera@gmail.com>
2023-07-25 21:16:43 +0100

    Added Testcontainers for tests

---
+++ src/test/java/com/challenge/challenge/BaseTest.java
@@ -0,0 +1,17 @@
+package com.challenge.challenge;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureMockMvc
+public abstract class BaseTest {
+
+    @Autowired
+    protected MockMvc mvc;
+
+}
